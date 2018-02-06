831dc063d100eb30316a4aa17380512cfffc9a60
Joe Antongiovanni <joe@Joes-Mac.local>
2018-02-05 23:10:21 -0800

    adding project, pod files, and readme

---
+++ flix_demo_03/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flix_demo_03
+//
+//  Created by Joe Antongiovanni on 2/5/18.
+//  Copyright © 2018 Joseph Antongiovanni. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
