a95ec4b92cd88de8c5203c09f21782c4605d02a1
Joe Antongiovanni <joe@Joes-Mac.local>
2018-02-07 15:01:48 -0800

    collection view

---
+++ flix_demo_03/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_demo_03
+//
+//  Created by Joe Antongiovanni on 2/7/18.
+//  Copyright © 2018 Joseph Antongiovanni. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
